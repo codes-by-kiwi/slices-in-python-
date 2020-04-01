4f9eb356b0c886524563a3cfc24accd353b8f523
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-04-01 13:41:29 +0200

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,7 @@
+fancystring = "I want to party till I drop!"
+print(fancystring[2:8]) # from index 2 to index 7
+print(fancystring[:6]) #from beginning to index 6
+print(fancystring[4:]) #from index 4 to the end 
+print(fancystring[-4]) #4th character from the end 
+print(fancystring[3:7:2]) #takes from index 3 to index 6 and does an increment step of 2.The general form is [start:stop:step].
+print(fancystring[::-1]) #[::-1] means start at the end of the string and end at position 0, move with the step -1 which means one step backwards.
